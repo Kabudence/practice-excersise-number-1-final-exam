40aabd16c0dc3cfa36394229dd3fbcc57e54a262
Angel Velasquez <pcisavel@upc.edu.pe>
2024-05-20 22:04:08 -0500

    chore: added generic model types.

---
+++ src/main/java/com/acme/center/platform/shared/domain/model/aggregates/AuditableAbstractAggregateRoot.java
@@ -0,0 +1,31 @@
+package com.acme.center.platform.shared.domain.model.aggregates;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.domain.AbstractAggregateRoot;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.Date;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public class AuditableAbstractAggregateRoot<T extends AbstractAggregateRoot<T>> {
+
+    @Id
+    @Getter
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Getter
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private Date createdAt;
+
+    @Getter
+    @LastModifiedDate
+    @Column(nullable = false)
+    private Date updatedAt;
+
+}
