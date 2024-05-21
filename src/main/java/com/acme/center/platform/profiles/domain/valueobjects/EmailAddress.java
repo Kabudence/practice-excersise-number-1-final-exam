96355a8d8722fbe59411668d8b28b1c71aa57962
Angel Velasquez <pcisavel@upc.edu.pe>
2024-05-20 22:04:36 -0500

    feat(profiles): added profile value objects.

---
+++ src/main/java/com/acme/center/platform/profiles/domain/valueobjects/EmailAddress.java
@@ -0,0 +1,9 @@
+package com.acme.center.platform.profiles.domain.valueobjects;
+
+import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.Email;
+
+@Embeddable
+public record EmailAddress(@Email String address) {
+    public EmailAddress() { this(null); }
+}
---
+++ src/main/java/com/acme/center/platform/profiles/domain/valueobjects/PersonName.java
@@ -0,0 +1,20 @@
+package com.acme.center.platform.profiles.domain.valueobjects;
+
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public record PersonName(String firstName, String lastName) {
+
+    public PersonName() { this(null, null); }
+
+    public PersonName {
+        if (firstName == null || firstName.isBlank())
+            throw new IllegalArgumentException("First name cannot be null or blank");
+        if (lastName == null || lastName.isBlank())
+            throw new IllegalArgumentException("Last name cannot be null or blank");
+    }
+
+    public String getFullName() {
+        return String.format("%s %s", firstName, lastName);
+    }
+}
