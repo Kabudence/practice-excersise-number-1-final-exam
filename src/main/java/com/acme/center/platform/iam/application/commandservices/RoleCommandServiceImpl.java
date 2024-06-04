43ead2d99229ec9231487e7da821120e022359e9
Angel Velasquez <pcisavel@upc.edu.pe>
2024-06-03 22:16:17 -0500

    feat(iam): added application layer role and user command services implementation.

---
+++ src/main/java/com/acme/center/platform/iam/application/commandservices/RoleCommandServiceImpl.java
@@ -0,0 +1,28 @@
+package com.acme.center.platform.iam.application.commandservices;
+
+import com.acme.center.platform.iam.domain.model.commands.SeedRolesCommand;
+import com.acme.center.platform.iam.domain.model.entities.Role;
+import com.acme.center.platform.iam.domain.model.valueobjects.Roles;
+import com.acme.center.platform.iam.domain.services.RoleCommandService;
+import com.acme.center.platform.iam.infrastructure.persistence.jpa.repositories.RoleRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+
+@Service
+public class RoleCommandServiceImpl implements RoleCommandService {
+    private final RoleRepository roleRepository;
+
+    public RoleCommandServiceImpl(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    @Override
+    public void handle(SeedRolesCommand command) {
+        Arrays.stream(Roles.values()).forEach(role -> {
+            if(!roleRepository.existsByName(role)) {
+                roleRepository.save(new Role(Roles.valueOf(role.name())));
+            }
+        });
+    }
+}
--- src/main/java/com/acme/center/platform/iam/domain/model/entities/Role.java
+++ src/main/java/com/acme/center/platform/iam/domain/model/entities/Role.java
@@ -2,15 +2,18 @@ package com.acme.center.platform.iam.domain.model.entities;
 
 import com.acme.center.platform.iam.domain.model.valueobjects.Roles;
 import jakarta.persistence.*;
+import lombok.Getter;
 
 import java.util.List;
 
 @Entity
 public class Role {
+    @Getter
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Getter
     @Enumerated(EnumType.STRING)
     @Column(length = 20, unique = true, nullable = false)
     private Roles name;
