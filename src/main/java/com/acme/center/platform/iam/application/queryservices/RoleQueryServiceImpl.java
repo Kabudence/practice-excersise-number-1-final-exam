ffd15ce1eba2b6509648c9141b7dab0facbdd8d6
Angel Velasquez <pcisavel@upc.edu.pe>
2024-06-03 22:21:25 -0500

    feat(iam): added application layer role and user query services implementation.

---
+++ src/main/java/com/acme/center/platform/iam/application/queryservices/RoleQueryServiceImpl.java
@@ -0,0 +1,30 @@
+package com.acme.center.platform.iam.application.queryservices;
+
+import com.acme.center.platform.iam.domain.model.entities.Role;
+import com.acme.center.platform.iam.domain.model.queries.GetAllRolesQuery;
+import com.acme.center.platform.iam.domain.model.queries.GetRoleByNameQuery;
+import com.acme.center.platform.iam.domain.services.RoleQueryService;
+import com.acme.center.platform.iam.infrastructure.persistence.jpa.repositories.RoleRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class RoleQueryServiceImpl implements RoleQueryService {
+    private final RoleRepository roleRepository;
+
+    public RoleQueryServiceImpl(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    @Override
+    public List<Role> handle(GetAllRolesQuery query) {
+        return roleRepository.findAll();
+    }
+
+    @Override
+    public Optional<Role> handle(GetRoleByNameQuery query) {
+        return roleRepository.findByName(query.roleName());
+    }
+}
---
+++ src/main/java/com/acme/center/platform/iam/application/queryservices/UserQueryServiceImpl.java
@@ -0,0 +1,36 @@
+package com.acme.center.platform.iam.application.queryservices;
+
+import com.acme.center.platform.iam.domain.model.aggregates.User;
+import com.acme.center.platform.iam.domain.model.queries.GetAllUsersQuery;
+import com.acme.center.platform.iam.domain.model.queries.GetUserByIdQuery;
+import com.acme.center.platform.iam.domain.model.queries.GetUserByUsernameQuery;
+import com.acme.center.platform.iam.domain.services.UserQueryService;
+import com.acme.center.platform.iam.infrastructure.persistence.jpa.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserQueryServiceImpl implements UserQueryService {
+    private final UserRepository userRepository;
+
+    public UserQueryServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public List<User> handle(GetAllUsersQuery query) {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public Optional<User> handle(GetUserByIdQuery query) {
+        return userRepository.findById(query.userId());
+    }
+
+    @Override
+    public Optional<User> handle(GetUserByUsernameQuery query) {
+        return userRepository.findByUsername(query.username());
+    }
+}
