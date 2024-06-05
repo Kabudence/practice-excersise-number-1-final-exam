5e989ddb9c98630ed0dc5619a4cd2d3017218534
Angel Velasquez <pcisavel@upc.edu.pe>
2024-06-05 14:50:02 -0500

    feat(iam): added user aggregate inbound service.

---
+++ src/main/java/com/acme/center/platform/iam/interfaces/rest/UsersController.java
@@ -0,0 +1,64 @@
+package com.acme.center.platform.iam.interfaces.rest;
+
+import com.acme.center.platform.iam.domain.model.queries.GetAllUsersQuery;
+import com.acme.center.platform.iam.domain.model.queries.GetUserByIdQuery;
+import com.acme.center.platform.iam.domain.services.UserQueryService;
+import com.acme.center.platform.iam.interfaces.rest.resources.UserResource;
+import com.acme.center.platform.iam.interfaces.rest.transform.UserResourceFromEntityAssembler;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * This class is a REST controller that exposes the users resource.
+ * It includes the following operations:
+ * - GET /api/v1/users: returns all the users
+ * - GET /api/v1/users/{userId}: returns the user with the given id
+ **/
+@RestController
+@RequestMapping(value = "/api/v1/users", produces = MediaType.APPLICATION_JSON_VALUE)
+@Tag(name = "Users", description = "User Management Endpoints")
+public class UsersController {
+    private final UserQueryService userQueryService;
+
+    public UsersController(UserQueryService userQueryService) {
+        this.userQueryService = userQueryService;
+    }
+
+    /**
+     * This method returns all the users.
+     * @return a list of user resources
+     * @see UserResource
+     */
+    @GetMapping
+    public ResponseEntity<List<UserResource>> getAllUsers() {
+        var getAllUsersQuery = new GetAllUsersQuery();
+        var users = userQueryService.handle(getAllUsersQuery);
+        var userResources = users.stream().map(UserResourceFromEntityAssembler::toResourceFromEntity).toList();
+        return ResponseEntity.ok(userResources);
+    }
+
+    /**
+     * This method returns the user with the given id.
+     * @param userId the user id
+     * @return the user resource with the given id
+     * @throws RuntimeException if the user is not found
+     * @see UserResource
+     */
+    @GetMapping(value = "/{userId}")
+    public ResponseEntity<UserResource> getUserById(@PathVariable Long userId) {
+        var getUserByIdQuery = new GetUserByIdQuery(userId);
+        var user = userQueryService.handle(getUserByIdQuery);
+        if (user.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        var userResource = UserResourceFromEntityAssembler.toResourceFromEntity(user.get());
+        return ResponseEntity.ok(userResource);
+    }
+}
