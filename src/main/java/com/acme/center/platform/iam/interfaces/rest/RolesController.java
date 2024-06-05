947635895671cdd22d6035dbeb2be636689ca0e6
Angel Velasquez <pcisavel@upc.edu.pe>
2024-06-05 14:49:51 -0500

    feat(iam): added role entity inbound service.

---
+++ src/main/java/com/acme/center/platform/iam/interfaces/rest/RolesController.java
@@ -0,0 +1,42 @@
+package com.acme.center.platform.iam.interfaces.rest;
+
+import com.acme.center.platform.iam.domain.model.queries.GetAllRolesQuery;
+import com.acme.center.platform.iam.domain.services.RoleQueryService;
+import com.acme.center.platform.iam.interfaces.rest.resources.RoleResource;
+import com.acme.center.platform.iam.interfaces.rest.transform.RoleResourceFromEntityAssembler;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ *  Roles Controller
+ *  This controller is responsible for handling all the requests related to roles
+ */
+@RestController
+@RequestMapping(value = "/ap/v1/roles", produces = MediaType.APPLICATION_JSON_VALUE)
+@Tag(name = "Roles", description = "Role Management Endpoints")
+public class RolesController {
+    private final RoleQueryService roleQueryService;
+
+    public RolesController(RoleQueryService roleQueryService) {
+        this.roleQueryService = roleQueryService;
+    }
+
+    /**
+     * Get all roles
+     * @return List of role resources
+     * @see RoleResource
+     */
+    @GetMapping
+    public ResponseEntity<List<RoleResource>> getAllRoles() {
+        var getAllRolesQuery = new GetAllRolesQuery();
+        var roles = roleQueryService.handle(getAllRolesQuery);
+        var roleResources = roles.stream().map(RoleResourceFromEntityAssembler::toResourceFromEntity).toList();
+        return ResponseEntity.ok(roleResources);
+    }
+}
